cb16cabf8976293855946a7b3ba1785330373ac2
Dea Sustaita <deasustaita@gmail.com>
2025-02-11 12:53:25 -0500

    moved some html files out of the login into the greater project

--- login/urls.py
+++ login/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('landing_page/', views.landing_page, name='landing_page'),  # Root URL for landing page
-    path('about/', views.about, name='about'),
+
     path('thinkeat/', views.thinkeat, name='thinkeat'),
     path('my-account/', views.my_account, name='my_account'),
 ]
--- software_project/urls.py
+++ software_project/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth import views as auth_views
 from login import views
 
 urlpatterns = [
+    path('landing_page/', views.landing_page, name='landing_page'),  # Root URL for landing page
+    path('about/', views.about, name='about'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),  # Include auth URLs
     path('', RedirectView.as_view(url='/accounts/login/')),  # Redirect root to login
