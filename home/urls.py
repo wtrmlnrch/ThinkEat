f22877f6edb1b70182a6465c752c2784f050e827
Dea Sustaita <deasustaita@gmail.com>
2025-02-13 15:21:20 -0500

    finished reorganizing structure

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('thinkeat/', views.thinkeat, name='thinkeat'),
     path('my-account/', views.my_account, name='my_account'),
 ]
