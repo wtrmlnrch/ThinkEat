5d8d70b33c98edaf7f26b8e397889c8376e0a337
Dea Sustaita <deasustaita@gmail.com>
2025-02-16 20:24:19 -0500

    added registration for the user

--- home/models.py
+++ home/models.py
@@ -2,3 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    birth_date = models.DateField(null=True, blank=True)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
 
 def landing_page(request):
     return render(request, 'home_page/landing_page.html')
@@ -15,12 +19,43 @@ def my_account(request):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['text']
+        username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect(landing_page)
+        
+        if not User.objects.filter(username=username).exists():
+            messages.error(request, "Invalid Username")
+            return redirect('/login/')
+        user = authenticate(username=username,password=password)
+
+        if user is None:
+            messages.error(request, "Invalid Password")
+            return redirect('/login/')
         else:
-            return render(request, 'login.html', {'error': 'Nuhuh'})
+            login(request, user)
+            return redirect('/landing_page/')
+        
     return render(request, 'login.html')
+
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email_address = request.POST.get('email_address')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.info(request, "Username already taken!")
+            return redirect('/register/')
+        
+        user = User.objects.create_user(first_name=first_name, last_name=last_name,username=username)
+
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, "Account created successfully!")
+        return render(request, 'registration/registration.html')
+    
+    return render(request, 'registration/registration.html')
