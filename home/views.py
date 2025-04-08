3022ad5a78b951c58f85461ba7a902fecade8964
Dea Sustaita <deasustaita@gmail.com>
2025-04-08 15:32:24 -0400

    added birthday to registration, and updated my-account to allow users to change their information.

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .models import *
+from .forms import CustomUserUpdateForm
 
 def landing_page(request):
     return render(request, 'home_page/landing_page.html')
@@ -14,7 +15,18 @@ def thinkeat(request):
     return render(request, 'thinkeat.html')
 
 def my_account(request):
-    return render(request, 'registration/my_account.html')
+    print("Request Method:", request.method)
+    request.session['test'] = 'Hello, world!'  # Store a test variable in the session
+    print("Session variable:", request.session.get('test'))  # Debugging line
+    
+    if request.method == 'POST':
+        form = CustomUserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid() and form.has_changed():
+            form.save()
+            messages.success(request, "Your information has been successfully updated.")
+    else:
+        form = CustomUserUpdateForm(instance=request.user)
+    return render(request, 'registration/my_account.html', {'form':form})
 
 def user_login(request):
     if request.method == 'POST':
@@ -42,7 +54,7 @@ def register(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email_address')  
-
+        birth_date = request.POST.get('birth_date')
        
         if not all([first_name, last_name, username, password, email]):
             messages.error(request, "All fields are required!")
@@ -51,12 +63,19 @@ def register(request):
             messages.error(request, "Username already taken!")
             return render(request, 'registration/registration.html', status=400)
 
+        try:
+            birth_date_parsed = datetime.datetime.strptime(birth_date, "%Y-%m-%d").date()
+        except ValueError:
+            messages.error(request, "Invalid birth date format.")
+            return render(request, 'registration/registration.html', status=400)
+
         CustomUser.objects.create_user(
             username=username,
             password=password,
             email=email,
             first_name=first_name,
-            last_name=last_name
+            last_name=last_name,
+            birth_date=birth_date_parsed
         )
         messages.success(request, "Account created successfully! Please log in.")
         return redirect('/login/')
--- software_project/settings.py
+++ software_project/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 WSGI_APPLICATION = 'software_project.wsgi.application'
 
